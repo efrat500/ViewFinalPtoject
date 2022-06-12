eba312e47999155b736b55c653068a1717b555c7
Shahar Harel Weiss (sharelwe) <sharelwe@cisco.com>
2022-06-12 03:18:36 +0300

    appUP

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import SignInScreen from './src/screens/Startscreen/SignInScreen';
+import StoriesOption from './src/screens/StoriesOptions';
 
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <SignInScreen />
+      <StoriesOption></StoriesOption>
     </SafeAreaView>
   );
 }
